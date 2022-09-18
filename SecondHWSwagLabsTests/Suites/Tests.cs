bd84f7b57a885fa3986a046da7158a9336de3bde
al.akhmadullin <al.akhmadullin@bars-open.ru>
2022-09-18 16:52:39 +0300

    Добавил метод ожидание, обернул в метод в классе Utilities

--- SecondHWSwagLabsTests/Suites/Tests.cs
+++ SecondHWSwagLabsTests/Suites/Tests.cs
@@ -2,12 +2,13 @@ using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
+using System.Collections.Generic;
 
 namespace SecondHWSwagLabsTests
 {
     public class Tests
     {
-        private IWebDriver driver;
+        public static IWebDriver driver;
 
         #region Locators
         private readonly By _loginField = By.Id("user-name");
@@ -39,36 +40,44 @@ namespace SecondHWSwagLabsTests
         public void AllTests()
         {
             // Авторизация
+            Utilities.Wait(new List<By> { _loginField, _passwordField, _loginButton });
             driver.FindElement(_loginField).SendKeys(Utilities.login);
             driver.FindElement(_passwordField).SendKeys(Utilities.password);
             driver.FindElement(_loginButton).Click();
             Console.WriteLine("Авторизация прошла успешно");
 
             // Добавление товара в корзину
+            Utilities.Wait(new List<By> { _addJacketToCartButton });
             driver.FindElement(_addJacketToCartButton).Click();
             Console.WriteLine("Добавлен товар в корзину: Sauce Labs Fleece Jacket");
 
             // Открытие корзины
+            Utilities.Wait(new List<By> { _shoppingCartBadgeButton });
             driver.FindElement(_shoppingCartBadgeButton).Click();
             Console.WriteLine("Переход в корзину");
 
             // Переход обратно в каталог (Продолжить покупки)
+            Utilities.Wait(new List<By> { _continueShoppingButton });
             driver.FindElement(_continueShoppingButton).Click();
             Console.WriteLine("Переход обратно в каталог");
 
             // Добавление товара в корзину
+            Utilities.Wait(new List<By> { _addShirtToCartButton });
             driver.FindElement(_addShirtToCartButton).Click();
             Console.WriteLine("Добавлен товар в корзину: Sauce Labs Bolt T-Shirt");
 
             // Открытие корзины
+            Utilities.Wait(new List<By> { _shoppingCartBadgeButton });
             driver.FindElement(_shoppingCartBadgeButton).Click();
             Console.WriteLine("Переход в корзину");
 
             // Перейти к оплате
+            Utilities.Wait(new List<By> { _checkoutButton });
             driver.FindElement(_checkoutButton).Click();
             Console.WriteLine("Переход к оплате");
 
             // Заполнение информации о покупателе и нажатие Continue
+            Utilities.Wait(new List<By> { _firstNameField, _lastNameField, _postalcodeField, _continueButton });
             driver.FindElement(_firstNameField).SendKeys(Utilities.firstName);
             driver.FindElement(_lastNameField).SendKeys(Utilities.lastName);
             driver.FindElement(_postalcodeField).SendKeys(Utilities.postalCode);
@@ -76,6 +85,7 @@ namespace SecondHWSwagLabsTests
             Console.WriteLine("Заполнение информации о покупателе и нажатие Continue");
 
             // Завершение покупки
+            Utilities.Wait(new List<By> { _finishButton });
             driver.FindElement(_finishButton).Click();
             Console.WriteLine("Завершение покупки");
         }
--- SecondHWSwagLabsTests/Utilities.cs
+++ SecondHWSwagLabsTests/Utilities.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,12 +8,21 @@ namespace SecondHWSwagLabsTests
 {
     internal class Utilities
     {
-        public const string URL = "https://www.saucedemo.com/";
+        private static WebDriverWait wait = new WebDriverWait(Tests.driver, TimeSpan.FromSeconds(30));
 
+        public const string URL = "https://www.saucedemo.com/";
         public const string login = "performance_glitch_user";
         public const string password = "secret_sauce";
         public const string firstName = "Benjen";
         public const string lastName = "Stark";
         public const string postalCode = "Somewhere behind the ice wall";
+
+        public static void Wait(List<By> locators)
+        {
+            foreach (var locator in locators)
+            {
+                wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(locator));
+            }
+        }
     }
 }
